a9a97a1beb9b2aef6a97aae6cc8c64c0de506b07
Slava Osipov <skybright@mail.ru>
2023-11-02 22:15:21 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,20 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+        var i = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\rLoading ... " + process[i++]);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                break;
+            }
+            if (i == process.length) {
+                i = 0;
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/concurrent/ThreadStop.java
@@ -0,0 +1,10 @@
+package ru.job4j.concurrent;
+
+public class ThreadStop {
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+}
